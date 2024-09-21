24a39e41699e953b62b258a9fa1b9dac171546fb
rudra patel <rudra7042004@gmail.com>
2024-09-21 21:48:18 +0530

    groupanagram

---
+++ array/groupAnagram.py
@@ -0,0 +1,21 @@
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            ans[tuple(count)].append(s)
+        return list(ans.values()) 
+
+
+if __name__ == "__main__":
+    strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+    sol = Solution()
+    result = sol.groupAnagrams(strs)
+    print(result)
+
